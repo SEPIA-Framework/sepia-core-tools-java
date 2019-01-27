c1a85c24b328419627dee8cb58eb0cf244dd8612
Florian Quirin <info@bytemind.de>
2019-01-27 20:09:50 +0100

    updated ACTIONS with language switch

--- src/main/java/net/b07z/sepia/server/core/assistant/ACTIONS.java
+++ src/main/java/net/b07z/sepia/server/core/assistant/ACTIONS.java
@@ -32,7 +32,6 @@ public class ACTIONS {
 	final public static String OPEN_LIST = "open_list";				//tell client to open a user specific list, param.: "listInfo" : {"indexType":...}
 	final public static String EXTEND_REQUEST = "extend_request";		//tell client that this request will take longer and he should prepare for that
 	final public static String SHOW_ABORT_OPTION = "show_abort";		//tell client to show an abort option
-	//deprecated: final public static String SKIP_TTS = "skip_tts";			//tell client to prevent TTS sound output (best used together with PLAY_AUDIO)
 	final public static String PLAY_AUDIO_URL = "play_audio_url";			//tell client to play a single audio file from url, parameters: audio_url
 	final public static String PLAY_AUDIO_STREAM = "play_audio_stream";	//tell client to play a continuous audio stream, parameters: audio_url, audio_title, audio_img, audio_type
 	final public static String STOP_AUDIO_STREAM = "stop_audio_stream";	//tell client to stop the running audio stream
@@ -42,11 +41,11 @@ public class ACTIONS {
 	final public static String ALARM = "alarm";					//tell client to set an alarm, parameters: tbd
 	final public static String REMINDER = "reminder";			//tell client to set an reminder, parameters: tbd
 	final public static String APPOINTMENT = "appointment";		//tell client to set an appointment in the calendar, parameters: tbd
+	final public static String SWITCH_LANGUAGE = "switch_language";		//tell client to switch language, parameters: language_code
 
 	//possible next actions:
 	//public static String STREAMING_REQUEST = "streaming_request";		//tell the client that he has to expect an answer that gets streamed
 	//public static String GET_HTML_META_INFO = "get_html_meta_info";	//tell client to open an url, get meta info when the page is loaded and say it, parameters: meta_tags, add_answer
-	//public static String EXECUTE_CMD = "execute_cmd";					//tell client to execute an command, parameters: command
 	
 	//ACTION OPTIONS
 	final public static String SKIP_TTS = "skipTTS";
