3b2a38ab9f74b8d45c366f6bc893c2a2a5f63c6b
Florian Quirin <info@bytemind.de>
2019-09-05 22:47:31 +0200

    Update ThreadManager.java

--- src/main/java/net/b07z/sepia/server/core/tools/ThreadManager.java
+++ src/main/java/net/b07z/sepia/server/core/tools/ThreadManager.java
@@ -3,6 +3,8 @@ package net.b07z.sepia.server.core.tools;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,13 +26,38 @@ public class ThreadManager {
 	}
 	
 	/**
-	 * Run a given task ({@link ThreadManagerTask}) with parameters in a certain number of threads
-	 * and wait for completion or timeout.
+	 * Run a single task in one thread with no extras. Calls 'threadPool.shutdownNow' on timeout ... which is not guaranteed to actually kill the process I guess.
+	 * @param myTask - {@link ThreadManagerTask} implementation (has to overwrite run(Object o) method). Note: 'Object o' will be null in this case.
+	 * @param timeoutMs - wait at most this time (ms) (until threadPool.shutdownNow will be called) 
+	 * @return true if thread finished in time
+	 * @throws Exception
+	 */
+	public static boolean runSingleTask(ThreadManagerTask myTask, long timeoutMs) throws Exception {
+		//Create one executor
+		ExecutorService threadPool = Executors.newFixedThreadPool(1);
+		//Run
+		threadPool.execute(() -> { 
+			myTask.run(null);
+		});
+		//Await termination
+		try{
+			awaitTerminationAfterShutdown(threadPool, timeoutMs);
+			return true;
+			
+	    }catch (InterruptedException ex){
+	        ex.printStackTrace();
+	        return false;
+	    }
+	}
+	
+	/**
+	 * Run a given task ({@link ThreadManagerTask}) with different parameters in a certain number of threads
+	 * and wait for completion or timeout. Calls 'threadPool.shutdownNow' on timeout ... which is not guaranteed to actually kill the process I guess.
 	 * @param numOfThreads - number of threads to use
 	 * @param myTask - {@link ThreadManagerTask} implementation (has to overwrite run(Object o) method).
-	 * @param parameters - List of objects used in myTask (one for each task)
-	 * @param timeoutMs - wait at most this time (ms)
-	 * @return
+	 * @param parameters - List of objects used in myTask (one for each task). Cannot be null!
+	 * @param timeoutMs - wait at most this time (ms) (until threadPool.shutdownNow will be called)
+	 * @return true if thread finished in time
 	 * @throws Exception
 	 */
 	public static boolean runTasks(int numOfThreads, ThreadManagerTask myTask, List<?> parameters,
@@ -72,6 +99,27 @@ public class ThreadManager {
 	        Thread.currentThread().interrupt();
 	        throw ex;
 	    }
+	}
+	
+	/**
+	 * Run a task in background after certain delay.<br>
+	 * For a more advanced version of this check 'net.b07z.sepia.server.assist.workers.ServiceBackgroundTaskManager'.
+	 * @param delayMs - execute after this time in milliseconds
+	 * @param task - Runnable to run
+	 * @return a {@link ScheduledFuture}
+	 */
+	public static ScheduledFuture<?> scheduleTaskToRunOnceInBackground(long delayMs, Runnable task){
+		int corePoolSize = 1;
+	    final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(corePoolSize);
+	    executor.setRemoveOnCancelPolicy(true);
+	    ScheduledFuture<?> future = executor.schedule(() -> {
+	    	//run task and purge
+	    	task.run();
+	    	executor.purge();
+	    	executor.shutdown();
+	    	return;
+	    }, delayMs, TimeUnit.MILLISECONDS);
+	    return future;
 	}
 
 }
