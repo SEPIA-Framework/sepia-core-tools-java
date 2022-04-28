b8962c1ad968416feaec3025cd9e55bfd639b80c
Florian Quirin <info@bytemind.de>
2022-04-28 19:30:29 +0200

    Update ENVIRONMENTS.java

--- src/main/java/net/b07z/sepia/server/core/assistant/ENVIRONMENTS.java
+++ src/main/java/net/b07z/sepia/server/core/assistant/ENVIRONMENTS.java
@@ -11,7 +11,7 @@ public class ENVIRONMENTS {
 	final public static String DEFAULT = "default"; 				//default environment with all options to display results, e.g. smartphone
 	final public static String AVATAR_DISPLAY = "avatar_display";	//avatar display with limited space to display results, sound available
 	final public static String SPEAKER = "speaker";					//smart speaker with audio only
-	final public static String SMART_SPEAKER = "speaker";			//smart speaker with audio only
+	final public static String SMART_SPEAKER = "smart_speaker";		//smart speaker with audio only
 	final public static String SMART_DISPLAY = "smart_display";		//smart speaker with display and audio
 	final public static String SILENT_DISPLAY = "silent_display";	//display without audio
 	final public static String CAR_DISPLAY = "car_display";			//display with audio. Might have modified behavior to for events, follow-ups, etc. TBD
